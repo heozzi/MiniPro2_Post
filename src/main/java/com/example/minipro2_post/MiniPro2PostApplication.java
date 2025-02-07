db79a9ca4bf98f111bc57a1dce5b2662308c4a53
heozzi <hz0819@naver.com>
2025-02-07 16:19:09 +0900

    Feat:First Commit

---
+++ src/main/java/com/example/minipro2_post/MiniPro2PostApplication.java
@@ -0,0 +1,13 @@
+package com.example.minipro2_post;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniPro2PostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniPro2PostApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/minipro2_post/MiniPro2PostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.minipro2_post;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniPro2PostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
